71afbef53c40ac509411a9a25a68ea4b5f59c3fb
Nityaa Singh <your_github_email@example.com>
2025-09-08 11:37:11 +0530

    Your descriptive commit message here

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname,"/public")));
 
 // Root route
 app.get("/", (req, res) => {
-  res.send("Hi, I am root");
+  res.redirect("/listings");
 });
 
 // Index Route
